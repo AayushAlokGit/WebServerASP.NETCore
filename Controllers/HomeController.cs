923c416f39a39ce3732c3664f8cf034b747c5b14
Aayush Alok <aayushalok@microsoft.com>
2025-02-23 12:01:54 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Web_API_Project.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloController : ControllerBase
+{
+
+    private readonly ILogger<HelloController> _logger;
+
+    public HelloController(ILogger<HelloController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public string Hello()
+    {
+        return "Hello from server";
+    }
+}
